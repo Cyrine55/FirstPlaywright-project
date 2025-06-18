dca1ac777f87d58b5224cee2609cfb7c4b26c70d
yefcyrine <yefrni.syrine@gmail.com>
2025-06-18 14:59:04 +0100

    navigation functions

--- tests/playwright-test.spec.ts
+++ tests/playwright-test.spec.ts
@@ -6,4 +6,16 @@ await page.goto('https://playwright.dev/');
 const title=await page.title();
 console.log(title);
 expect(title).toContain('Playwright');
+
+await page.goto("http://gmail.com");
+await page.waitForTimeout(2000);
+
+await page.goBack();
+await page.waitForTimeout(2000);
+
+await page.goForward();
+await page.waitForTimeout(2000);
+
+await page.reload();
+await page.waitForTimeout(2000);
 });
